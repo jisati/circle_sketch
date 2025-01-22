d9af86183fe0a67c473902a635d28cbfc0b67ce7
jisati <j.shinkeiii@gmail.com>
2025-01-22 17:32:38 -0500

    Add files via upload

---
+++ newcircle_sketch/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  if(mouseIsPressed === true) {
+    fill(0);
+    
+    
+  }else {
+    fill(2550);
+  }
+  
+  
+  // blah a circle 
+  
+  circle(mouseX,mouseY,100);
+}
